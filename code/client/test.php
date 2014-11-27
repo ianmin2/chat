f4c3310685afd671f6f75b9b096c49bf446a3bf9
Ian Innocent <ianmin2@live.com>
2014-11-27 20:53:46 +0300

    moved folders

---
+++ code/client/test.php
@@ -0,0 +1,17 @@
+<?php
+
+	/* FILE PROCESSING LAYOUT */
+	/*
+	
+	1.$argv
+		=> [0]   = 	filename
+		=> [1] 	 = 	action to take e.g [
+							addUser
+							doLogin
+						   ]		
+	
+	/*EO FILE LAYOUT*/
+
+$argv;
+
+?>
